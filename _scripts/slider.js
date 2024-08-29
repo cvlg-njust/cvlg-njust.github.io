3186a945441e1d0cd8614db0d9f170c99189d92b
cvlg-njust <njust_cvlg@163.com>
2024-08-29 12:47:30 +0000

    Deploying to gh-pages from @ cvlg-njust/cvlg-njust.github.io@982d394a5764d4a74e49fbbc75fcb4079d74909f 🚀

---
+++ _scripts/slider.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+
+function changeSlide(n) {
+  const slides = document.querySelectorAll('.slider-item');
+  const totalSlides = slides.length;
+  const wrapper = document.querySelector('.slider-wrapper');
+  const itemsToShow = 3; // 每次显示三张图片
+
+  slideIndex += n;
+
+  // 循环播放
+  if (slideIndex < 0) {
+    slideIndex = totalSlides - itemsToShow;
+  } else if (slideIndex >= totalSlides - itemsToShow + 1) {
+    slideIndex = 0;
+  }
+
+  const newTransform = `translateX(-${(slideIndex * 100) / itemsToShow}%)`;
+  wrapper.style.transform = newTransform;
+}
