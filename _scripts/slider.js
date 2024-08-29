52b58d5437a3702f2c8bde6d9e2e72d0cbc69e84
cvlg-njust <njust_cvlg@163.com>
2024-08-29 13:20:53 +0000

    Deploying to gh-pages from @ cvlg-njust/cvlg-njust.github.io@f9be07a258d80a6c7acd5fead13549f5f53107cb 🚀

--- _scripts/slider.js
+++ _scripts/slider.js
@@ -1,20 +1,30 @@
-let slideIndex = 0;
+let currentSlideIndex = 0;
 
-function changeSl(n) {
-  const slides = document.querySelectorAll('.slider-item');
-  const totalSlides = slides.length;
-  const wrapper = document.querySelector('.slider-wrapper');
+function changeSl(direction) {
+  const sliderWrapper = document.querySelector('.slider-wrapper');
+  const sliderItems = document.querySelectorAll('.slider-item');
+  const totalSlides = sliderItems.length;
   const itemsToShow = 3; // 每次显示三张图片
+  const itemWidth = sliderItems[0].getBoundingClientRect().width + 10; // 获取每个图片项的宽度，加上margin
 
-  slideIndex += n;
+  currentSlideIndex += direction;
 
-  // 循环播放
-  if (slideIndex < 0) {
-    slideIndex = totalSlides - itemsToShow;
-  } else if (slideIndex >= totalSlides - itemsToShow + 1) {
-    slideIndex = 0;
+  // 确保在滑动到最后一张或第一张时，循环播放
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = totalSlides - itemsToShow;
+  } else if (currentSlideIndex > totalSlides - itemsToShow) {
+    currentSlideIndex = 0;
   }
 
-  const newTransform = `translateX(-${(slideIndex * 100) / itemsToShow}%)`;
-  wrapper.style.transform = newTransform;
+  const newTransform = `translateX(-${currentSlideIndex * itemWidth}px)`;
+  sliderWrapper.style.transform = newTransform;
 }
+
+document.querySelector('.prev').addEventListener('click', function() {
+  changeSl(-1);
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+  changeSl(1);
+});
+
